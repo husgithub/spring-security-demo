ab103d4d6790950fe843e9576594e8e132d70048
husong <husong_163mailbox@163.com>
2019-07-13 18:35:30 +0800

    自定义登录页demo

--- 01-custom-login-page/src/main/java/com/example/demo/controller/IndexController.java
+++ 01-custom-login-page/src/main/java/com/example/demo/controller/IndexController.java
@@ -2,14 +2,33 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/index.html")
-    public String getArticleList(Model model) {
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("title", "首页");
         model.addAttribute("content", "这是一个SpringSecurity入门例子！");
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/admin/admin")
+    public String admin(Model model) {
+        model.addAttribute("title", "管理员页");
+        return "admin/admin";
+    }
+
+    @GetMapping("/user/user")
+    public String user(Model model) {
+        model.addAttribute("title", "用户页");
+        return "user/user";
     }
 }
